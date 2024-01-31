9dd848c4fa2ee9a1d1705cf35f59d201c2deebc0
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-01-31 14:25:22 +0330

    thread_join declared

--- ulib.c
+++ ulib.c
@@ -113,3 +113,10 @@ int thread_create(void (*start_routine)(void *, void *), void* arg1, void* arg2)
 
   return clone(start_routine, arg1, arg2, stack);
 }
+
+int thread_join()
+{
+  void * stackPtr;
+  int x = join(&stackPtr);
+  return x;
+}
